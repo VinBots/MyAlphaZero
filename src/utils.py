47248e4279c3d73434d20d8b836ad124e24627b1
Vincent Manier <vincentmanier@hotmail.com>
2021-05-19 20:16:05 -0400

    typo

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,5 @@
 class DotDict(dict):
-    def __getattr__(self, name):        
-        #return self[name]
-        
+    def __getattr__(self, name):                
         try:
             return self[name]
         except KeyError:
